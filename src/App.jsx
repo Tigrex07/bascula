6792a21aee4f7f67b9e71a52263c7e8c49cd7d48
Tigrex07 <136181670+Tigrex07@users.noreply.github.com>
2026-03-12 12:32:11 -0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="bg-blue-500 text-white p-10 text-3xl">
+      Tailwind funcionando
+    </div>
+    </>
+  )
+}
+
+export default App
